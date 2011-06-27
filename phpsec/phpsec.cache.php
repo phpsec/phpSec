5ed109ee2c9b6a51b0faec1706ad42c616434b6b
Audun Larsen <larsen@xqus.com>
2011-06-27 20:49:40 +0200

    Fixed e-mail address.

--- phpsec/phpsec.cache.php
+++ phpsec/phpsec.cache.php
@@ -2,7 +2,7 @@
 /**
   phpSec - A PHP security library
 
-  @author    Audun Larsen <larsen@gmail.com>
+  @author    Audun Larsen <larsen@xqus.com>
   @copyright Copyright (c) Audun Larsen, 2011
   @link      https://github.com/xqus/phpSec
   @license   http://opensource.org/licenses/mit-license.php The MIT License
