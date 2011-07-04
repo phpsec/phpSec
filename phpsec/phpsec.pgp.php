1f940cb212fefd6557c35b70f36a38b8ff3c84c7
Audun Larsen <larsen@xqus.com>
2011-07-05 01:37:11 +0200

    Documented the functions, and the different params.

--- phpsec/phpsec.pgp.php
+++ phpsec/phpsec.pgp.php
@@ -13,27 +13,88 @@
  * Class to act as an interface to GPG.
  */
 class phpsecPgp {
-  public static $_gpgPath = '/var/bin/gpg';
-  public static $_keyDir = null;
+  public static $_gpgPath = '/usr/bin/gpg';
 
+  /* Directory where we will store the keys. */
+  public static $_keyDir  = null;
 
-  public static function genKeys() {
+  /* Private key we want to use. */
+  public static $privKey  = null;
+
+  /* Passphrase for the private key we want to use. */
+  public static $keyPass  = null;
+
+  /**
+   * Generate a pair of keys.
+   *
+   * @param string $name
+   *   Name of the owner of the key.
+   *
+   * @param string $email
+   *   E-mail to the owner of the key.
+   *
+   * @param string $comment
+   *   Key comment.
+   *
+   * @param string $passphrase
+   *   Passphrase to protect the privarte key with.
+   *
+   * @return string
+   *   Key ID.
+   */
+  public static function genKeys($name, $email, $comment, $passphrase) {
 
   }
 
-  public static function signKey() {
+  /**
+   * Sign a public key with a private key.
+   *
+   * @param string $key
+   *   Name of public key to sign.
+   *
+   * @return bool
+   *   True on success, false on error.
+   */
+  public static function signKey($key) {
 
   }
 
-  public static  function sign() {
+  /**
+   * Sign data.
+   *
+   * @param string $data
+   *   Data to sign.
+   *
+   * @return string
+   *   Signature.
+   */
+  public static function sign($data) {
 
   }
 
-  public static function encrypt() {
+  /**
+   * Encrypt data.
+   *
+   * @param string $data
+   *   Data to encrypt.
+   *
+   * @return string
+   *   Encrypted data.
+   */
+  public static function encrypt($data) {
 
   }
 
-  public static function decrypt() {
+  /**
+   * Decrypt data.
+   *
+   * @param string $data
+   *   Data to decrypt.
+   *
+   * @return string
+   *   Decrypted data.
+   */
+  public static function decrypt($data) {
 
   }
 
