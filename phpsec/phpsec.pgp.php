90b17010b4b74a8bb1ca4f4295f9d143a5ad5c4a
Audun Larsen <larsen@xqus.com>
2011-07-10 16:38:08 +0200

    Create keypairs.

--- phpsec/phpsec.pgp.php
+++ phpsec/phpsec.pgp.php
@@ -43,7 +43,40 @@ class phpsecPgp {
    *   Key ID.
    */
   public static function genKeys($name, $email, $comment, $passphrase) {
+    $descriptorspec = array(
+      0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+      1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+      2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
+    );
 
+    $process = proc_open(self::prep('batch --gen-key'), $descriptorspec, $pipes);
+    if (is_resource($process)) {
+
+      /* Setup the key data. */
+      $str = "Key-Type: DSA\n" .
+             "Key-Length: 1024\n" .
+             "Subkey-Type: ELG-E\n" .
+             "Subkey-Length: 1024\n" .
+             "Name-Real: ".$name."\n" .
+             "Name-Comment: ".$comment."\n" .
+             "Name-Email: ".$email."\n" .
+             "Expire-Date: 0\n" .
+             "Passphrase: ".$passphrase."\n" .
+             "%commit";
+
+      /* Write to STDIN. */
+      fwrite($pipes[0], $str);
+
+      /* Close it up, Scotty! */
+      fclose($pipes[0]);
+      fclose($pipes[1]);
+
+      /* Return true if all is good. */
+      if(proc_close($process) == 0) {
+        return true;
+      }
+    }
+    return false;
   }
 
   /**
@@ -72,8 +105,15 @@ class phpsecPgp {
 
   }
 
+  public static function verify($sig, $data) {
+
+  }
+
   /**
    * Encrypt data.
+   *
+   * @param string $to
+   *   Name of public key to the reciever.
    *
    * @param string $data
    *   Data to encrypt.
@@ -81,7 +121,7 @@ class phpsecPgp {
    * @return string
    *   Encrypted data.
    */
-  public static function encrypt($data) {
+  public static function encrypt($to, $data) {
 
   }
 
@@ -96,6 +136,18 @@ class phpsecPgp {
    */
   public static function decrypt($data) {
 
+  }
+
+  /**
+   * Create a GPG command ready to pass to shell_exec()
+   */
+  private static function prep($action, $options = array()) {
+    $str = self::$_gpgPath;
+    $str .= ' --homedir '.self::$_keyDir;
+
+    $str .= ' --'.$action;
+    echo $str;
+    return $str;
   }
 
 }
