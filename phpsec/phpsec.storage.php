55cfb8cabebc097304c2ddcc27aeb290b208cc0c
Audun Larsen <larsen@xqus.com>
2011-07-17 13:03:11 +0200

    Added storage class and interface.

---
+++ phpsec/phpsec.storage.php
@@ -0,0 +1,23 @@
+<?php
+/**
+  phpSec - A PHP security library
+
+  @author    Audun Larsen <larsen@xqus.com>
+  @copyright Copyright (c) Audun Larsen, 2011
+  @link      https://github.com/xqus/phpSec
+  @license   http://opensource.org/licenses/mit-license.php The MIT License
+  @package   phpSec
+ */
+
+class phpsecStorage {
+  const GC_PROB   = 0.2;
+  const HASH_TYPE = 'sha256';
+  
+  function __construct() {
+  	
+  }
+  
+  private static function gc() {
+   	
+  } 
+}
