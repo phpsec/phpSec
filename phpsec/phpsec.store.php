cb2496880a728e2603951bbf32d028f1d8745cf2
Audun Larsen <larsen@xqus.com>
2011-10-13 18:57:26 +0200

    #38 Abstract class desribing the phpSec storage interface class.

---
+++ phpsec/phpsec.store.php
@@ -0,0 +1,68 @@
+<?php
+/**
+  phpSec - A PHP security library
+
+  @author    Audun Larsen <larsen@xqus.com>
+  @copyright Copyright (c) Audun Larsen, 2011
+  @link      https://github.com/xqus/phpSec
+  @license   http://opensource.org/licenses/mit-license.php The MIT License
+  @package   phpSec
+ */
+
+/* Abstract class desribing the phpSec storage interface class. */
+abstract class phpsecStore {
+
+  /**
+   * Open/prepeare the storage.
+   *
+   * @param string $target
+   *   The exact storage target as configured by user.
+   *
+   * @return bool
+   *   Returns true on success and false on error.
+   */
+  abstract public function __construct($target);
+
+  /**
+   * Read data from storage.
+   *
+   * @param string $type
+   *   Type of data (session, cache, etc.).
+   *
+   * @param string $id
+   *   Unique identifier.
+   *
+   * @return mixed
+   *   Returns data.
+   */
+  abstract public function read($type, $id);
+
+  /**
+   * Write data to storeage.
+   *
+   * @param string $type
+   *   Type of data (session, cache, etc.).
+   *
+   * @param string $id
+   *   Unique identifier.
+   *
+   * @return bool
+   *   Returns true on success, false on error.
+   */
+  abstract public function write($type, $id);
+
+  /**
+   * Delete data from storeage.
+   *
+   * @param string $type
+   *   Type of data (session, cache, etc.).
+   *
+   * @param string $id
+   *   Unique identifier.
+   *
+   * @return bool
+   *   Returns true on success, false on error.
+   */
+  abstract public function delete($type, $id);
+
+}
