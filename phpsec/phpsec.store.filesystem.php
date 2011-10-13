d2d5d3f48da0e68e19d0bab1d0e04ab4209de42b
Audun Larsen <larsen@xqus.com>
2011-10-13 18:58:11 +0200

    #38 Filesystem storage class.

---
+++ phpsec/phpsec.store.filesystem.php
@@ -0,0 +1,14 @@
+<?php
+/**
+  phpSec - A PHP security library
+
+  @author    Audun Larsen <larsen@xqus.com>
+  @copyright Copyright (c) Audun Larsen, 2011
+  @link      https://github.com/xqus/phpSec
+  @license   http://opensource.org/licenses/mit-license.php The MIT License
+  @package   phpSec
+ */
+
+class phpsecStoreFilesystem extends phpsecStore {
+
+}
