b48a83f8b13268eaa14715e175ebee863da0b13b
Audun Larsen <larsen@xqus.com>
2011-12-19 17:11:02 +0100

    Added documentation and copyright.

--- phpsec/phpsec.store.pdo.php
+++ phpsec/phpsec.store.pdo.php
@@ -1,5 +1,17 @@
 <?php
+/**
+  phpSec - A PHP security library
 
+  @author    Audun Larsen <larsen@xqus.com>
+  @copyright Copyright (c) Audun Larsen, 2011
+  @link      https://github.com/phpsec/phpSec
+  @license   http://opensource.org/licenses/mit-license.php The MIT License
+  @package   phpSec
+ */
+
+/**
+ *  Class for handling database storage.
+ */
 class phpsecStorePdo extends phpsecStore {
 
   public  $_hashType = 'sha256';
