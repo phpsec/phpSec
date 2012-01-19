1ccdcbe990a38df6ad3cf1aa3282a402aaf478d1
Audun Larsen <larsen@xqus.com>
2012-01-19 17:56:24 +0100

    Update year in copyright.

--- phpsec/phpsec.hash.php
+++ phpsec/phpsec.hash.php
@@ -3,7 +3,7 @@
   phpSec - A PHP security library
 
   @author    Audun Larsen <larsen@xqus.com>
-  @copyright Copyright (c) Audun Larsen, 2011
+  @copyright Copyright (c) Audun Larsen, 2011, 2012
   @link      https://github.com/phpsec/phpSec
   @license   http://opensource.org/licenses/mit-license.php The MIT License
   @package   phpSec
